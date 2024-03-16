2c12a0ec1ee095ce5f5512a14fcf518b6ca5b3bc
kacmazeren <x22129219@student.ncirl.ie>
2024-03-16 16:12:43 +0000

    new project created due to plugin errors and all files copied to the new project.

--- app/src/main/java/com/eren/order/MainActivity.kt
+++ app/src/main/java/com/eren/order/MainActivity.kt
@@ -6,10 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.eren.order.ui.theme.OrderTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,11 +20,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             OrderTheme {
                 // A surface container using the 'background' color from the theme
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    PagePasses()
                 }
             }
         }
@@ -30,17 +33,19 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    OrderTheme {
-        Greeting("Android")
+fun PagePasses(){
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "menu_app"){
+        composable(route = "menu_app"){
+            MenuApp(navController)
+        }
+        composable(route = "order_screen"){
+            OrderScreen(navController)
+        }
     }
+}
+@Preview
+@Composable
+fun AppPreview(){
+    PagePasses()
 }
---
+++ app/src/main/java/com/eren/order/MenuScreen.kt
@@ -0,0 +1,129 @@
+package com.eren.order
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.eren.order.data.MenuProvider
+import com.eren.order.model.Food
+import com.eren.order.ui.theme.OrderTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MenuApp(navController: NavHostController) {
+    var food :Food
+    val orders = remember {
+        mutableStateListOf("")
+    }
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(5.dp)
+        ) {
+            Text(text = "MENU")
+
+            LazyColumn {
+                items(count = MenuProvider.getFoodData().size,
+                    itemContent = {
+                        food = MenuProvider.getFoodData()[it]
+
+                        MenuItemView(
+                            food = food,
+                            onItemClick = {orders.add(food.toString())}
+                        )
+                    })
+            }
+            Button(onClick = {
+                navController.navigate(route = "order_screen")
+            }) {
+                Text(text = "Continue")
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MenuItemView(food: Food,
+                 onItemClick: (Food) -> Unit,
+                 modifier: Modifier = Modifier,
+
+                 ) {
+    var checked by remember { mutableStateOf(false) }
+    Card(
+        elevation = CardDefaults.cardElevation(),
+        modifier = modifier,
+        onClick =  {   }
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(5.dp)
+                    .weight(1f)
+            ) {
+                Text(
+                    text = stringResource(food.foodName),
+                    style = MaterialTheme.typography.titleMedium,
+                    modifier = Modifier,
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .padding(5.dp)
+                    .weight(1f)
+            ) {
+                Text(
+                    text = stringResource(food.price),
+                    style = MaterialTheme.typography.bodySmall,
+                    modifier = Modifier
+                )
+            }
+            Checkbox(checked = checked, onCheckedChange ={isChecked ->
+                checked = isChecked} )
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun MenuPreview() {
+    OrderTheme {
+        PagePasses()
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun MenuItemViewPreview() {
+    OrderTheme {
+        MenuItemView(food= MenuProvider.getFoodData().component1(),
+            onItemClick = {})
+    }
+}
